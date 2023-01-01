10325eb43c14bf0056e98a56dae8091f3f4bcc2d
Amirreza Shahidi <shahidiam83@gmail.com>
2023-01-01 11:24:15 +0330

    add services' structures

---
+++ src/ManageAttributeValueService.php
@@ -0,0 +1,39 @@
+<?php
+namespace BardSoftware\LaravelAttributes;
+
+class ManageAttributeValueService{
+
+    public function addAttributeValue(){
+
+    }
+    
+    public function editAttributeValue(){
+
+    }
+
+    public function findAttributeValue(){
+
+    }
+
+    public function listAttributeValue(){
+
+    }
+
+    public function getValuesByAttribute($attribute){
+
+    }
+
+    public function getValuesByEntity($entity){
+
+    }
+
+    public function attachToAttribute(){
+
+    }
+
+    public function detachFromAttribute(){
+        
+    }
+}
+
+?>
---
+++ src/ManageAttributesService.php
@@ -0,0 +1,36 @@
+<?php
+namespace BardSoftware\LaravelAttributes;
+
+class ManageAttributesService{
+
+    public function addAttribute($attribute){
+
+    }
+
+    public function editAttribute($attribute_id,$attribute){
+
+    }
+
+    public function findAttribute($keyword){
+
+    }
+
+    public function listAttributes(){
+
+    }
+
+    public function getAttributesByEntity($entity){
+
+    }
+
+    public function attachToEntity($attribute,$entity){
+
+    }
+
+    public function detachFromEntity($attribute,$entity){
+        
+    }
+
+}
+
+?>
