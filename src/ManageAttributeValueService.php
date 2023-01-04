14cc135c5fc9b89c676261942aba8a414912c4e1
Amirreza Shahidi <shahidiam83@gmail.com>
2023-01-04 17:51:55 +0330

    fill service methods

--- src/ManageAttributeValueService.php
+++ src/ManageAttributeValueService.php
@@ -1,37 +1,44 @@
 <?php
 namespace BardSoftware\LaravelAttributes;
 
+use App\Models\AttributeValue;
+use App\Models\ValueEntity;
+
 class ManageAttributeValueService{
 
-    public function addAttributeValue(){
-
+    public function addAttributeValue($title,$attribute_id){
+        return AttributeValue::create(['title' => $title, 'attribute_id' => $attribute_id]);
     }
     
-    public function editAttributeValue(){
+    public function editAttributeValue($value_id,$key,$value){
+        AttributeValue::where('id',$attribute_id)->update([$attribute_key => $attribute_value]);
+        return AttributeValue::where('id',$attribute_id)->first();
 
     }
 
-    public function findAttributeValue(){
-
+    public function findAttributeValue($title){
+        return AttributeValue::where('title','LIKE',"%$title%")->get();
     }
 
-    public function listAttributeValue(){
-
+    public function listAttributeValue($page){
+        return AttributeValue::skip( ($page - 1) * 10 )->take(10)->get();
     }
 
-    public function getValuesByAttribute($attribute){
-
+    public function getValuesByAttribute($attribute_id){
+        return AttributeValue::where('attribute_id',$attribute_id)->get();
     }
 
-    public function getValuesByEntity($entity){
-
+    public function getValuesByEntity($entity_id){
+        return ValueEntity::where('entity_id',$entity_id)->get();
     }
 
-    public function attachToAttribute(){
-
+    public function attachToAttribute($value_id,$attribute_id){
+        $query = AttributeValue::where('id',$value_id);
+        $query->update(['attribute_id' => $attribute_id]);
+        return $query->first();
     }
 
-    public function detachFromAttribute(){
+    public function detachFromAttribute($attribute_id){
         
     }
 }
--- src/ManageAttributesService.php
+++ src/ManageAttributesService.php
@@ -1,34 +1,46 @@
 <?php
 namespace BardSoftware\LaravelAttributes;
 
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use App\Models\AttributeEntity;
+
 class ManageAttributesService{
 
-    public function addAttribute($attribute){
-
+    public function addAttribute($attribute_title,$attribute_slug,$attribute_type){
+        return Attribute::create(['title' => $attribute_title, 'slug' => $attribute_slug, 'type' => $attribute_type]);
     }
 
-    public function editAttribute($attribute_id,$attribute){
+    public function editAttribute($attribute_id,$attribute_key,$attribute_value){
+        Attribute::where('id',$attribute_id)->update([$attribute_key => $attribute_value]);
+        return Attribute::where('id',$attribute_id)->first();
+    }   
 
+    public function findAttribute($title){
+        return Attribute::where('title','LIKE',"%$title%")->get();
     }
 
-    public function findAttribute($keyword){
-
+    public function listAttributes($page){
+        return Attribute::skip( ($page - 1) * 10 )->take(10)->get();
     }
 
-    public function listAttributes(){
-
+    public function getAttributesByEntity($entity_id){
+        $attribute_ids = AttributeEntity::where('entity_id',$entity_id)->get()->only('attribute_id');
+        return Attribute::whereIn('id',$attribute_ids)->get();
     }
 
-    public function getAttributesByEntity($entity){
-
+    public function attachToEntity($attribute_id,$entity_id,$entity_type){
+        return AttributeEntity::create(
+            [
+                'attribute_id' => $attribute_id, 
+                'entity_id' => $entity_id , 
+                'entity_type' => $entity_type
+            ]
+        );
     }
 
-    public function attachToEntity($attribute,$entity){
-
-    }
-
-    public function detachFromEntity($attribute,$entity){
-        
+    public function detachFromEntity($attribute_id,$entity_id){
+        return AttributeEntity::where('attribute_id',$attribute_id)->where('entity_id',$entity_id)->delete();
     }
 
 }
